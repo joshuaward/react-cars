ec077e1fd405a85c2e02683f22f3d744c48aa730
Josh <joshua.ward@me.com>
2023-04-06 14:41:15 -0600

    first commit

---
+++ src/components/CarList.js
@@ -0,0 +1,45 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { removeCar } from '../store';
+
+function CarList() {
+  const dispatch = useDispatch();
+
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm }}) => {
+    const filteredCars = data.filter((car) => 
+      car.name
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase())
+    );
+
+    return {
+      cars: filteredCars,
+      name: form.name
+    }
+  });
+
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id))
+  };
+
+  const renderedCars = cars.map((car) => {
+    // decide if existing car should be bold
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
+    return (
+      <div className={`panel ${bold && 'bold'}`} key={car.id}>
+        <p>{car.name} - ${car.cost}</p>
+        <button className="button is-danger" onClick={() => handleCarDelete(car)}>Delete</button>
+      </div>
+    )
+  });
+  return (
+    <div>
+      <div className="car-list">
+        {renderedCars}
+        <hr />
+      </div>    
+    </div>
+  );
+}
+
+export default CarList;
